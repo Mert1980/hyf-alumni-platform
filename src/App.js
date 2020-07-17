01cb97fffd50db6834f6b630e3fc1c88ae414df7
Mert Demirok <mertdemirok80@gmail.com>
2020-07-17 06:39:14 +0200

    signup and signin with material design

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Homepage from "./components/landing/Homepage";
 
 function App() {
-  return <div>HYF Alumni Platform</div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/landing/Homepage.jsx
@@ -0,0 +1,74 @@
+// import React from "react";
+import Login from "../login/Login";
+import Signup from "../signup/Signup";
+
+// function Homepage() {
+//   return (
+//     // <SignIn />
+//     <Signup />
+//   );
+// }
+
+// export default Homepage;
+
+import React, { useState } from "react";
+import { Redirect, Router } from "react-router";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function Homepage() {
+  const classes = useStyles();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const homePage = (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Hackyourfuture Alumni Platform
+          </Typography>
+          <Button onClick={() => setLoggedIn(true)} color="inherit">
+            Login
+          </Button>
+          <Button color="inherit">Signup</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+
+  return (
+    <>
+      {loggedIn ? (
+        <Router>
+          <Redirect to="/login" />
+        </Router>
+      ) : (
+        homePage
+      )}
+    </>
+  );
+}
