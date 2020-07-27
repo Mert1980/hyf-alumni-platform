9d194a8d77e4af49786af7653dc46924fb897db6
HackYourFuture Belgium <hackyourfuture@debian>
2020-07-27 10:20:46 +0200

    tried a package but not working

--- src/components/profile/UploadCV.jsx
+++ src/components/profile/UploadCV.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Button, Container } from '@material-ui/core';
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import { Button, Container } from "@material-ui/core";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
@@ -13,16 +13,15 @@ export default function UploadCV() {
   return (
     <div>
       <Container spacing={1}>
-      <Button
-        variant="contained"
-        color="default"
-        className={classes.button}
-        startIcon={<CloudUploadIcon />}
-      >
-        Upload CV
-      </Button>
+        <Button
+          variant="contained"
+          color="default"
+          className={classes.button}
+          startIcon={<CloudUploadIcon />}
+        >
+          Upload CV
+        </Button>
       </Container>
-     
     </div>
   );
 }
